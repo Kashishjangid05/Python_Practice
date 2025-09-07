9d023d62196ef75f9c8978e5f1b5c98c97685d21
Kashish Jangid <kashishjangidkj@gmail.com>
2025-09-07 23:48:36 +0530

    Add files via upload

---
+++ Practice13.py
@@ -0,0 +1,10 @@
+#WAP to insert dictionary
+dict = {
+    "cat":"a small animal",
+    "table":["a table of furniture","list of facts and figures"]
+}
+print(dict)
+
+#WAP to find the number of classrooms of each subject
+set = {"python" , "java" ,"C++" , "python" , "javascript" , "java" , "python" , "java","C++" , "C"}
+print(len(set))
