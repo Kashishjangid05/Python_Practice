0b7cfae13e38c45027aeeb80ce046bf79694a47d
Kashishjangid05 <kashishjangidkj@gmail.com>
2025-09-18 22:52:50 +0530

    commit

---
+++ Basics/Oops.py
@@ -0,0 +1,25 @@
+# #class and object
+# class student:
+#     name = "Bittu"
+#     course = "B.Tech"
+
+# s1 = student()
+# print(s1.name)
+# print(s1.course)
+
+# init function
+# ---Cnstructor---
+class student:
+
+    # default constructors
+    def __init__(self):
+        pass
+
+    # parameterized constructors
+    def __init__(self , name , marks):
+        self.name = name
+        self.marks = marks
+        print("Kashish")
+    
+s1 = student("Bittu" , 98)
+print(s1.name , s1.marks)
